9b38fd00fd9f50fd74d50dc6945fa2178f654c08
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-02-15 19:13:17 +0900

    Update p124_DFS.py

--- 이코테/p124_DFS.py
+++ 이코테/p124_DFS.py
@@ -204,7 +204,7 @@ for i in range(graph):
 print("count :",count)
 
 
-'''
+
 # 책의 풀이
 # N,M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
@@ -252,3 +252,42 @@ for i in range(n):
       print("result + 1 =",result)
 
 print("결과 :",result)
+
+'''
+
+# 4 미로탈출 
+from collections import deque
+
+# N,M을 공백으로 구분하여 입력받기
+n, m = map(int , input().split())
+graph = []
+for i in range(n):
+  graph.append(list(map(int,input())))
+
+# 이동할 네 방향의 정의 (상, 하, 좌, 우)
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+# BFS 소스코드 구현
+def bfs(x,y):
+  # 큐 구현을 위해 deque 라이브러리 사용
+  queue = deque()
+  queue.append((x,y))
+
+  while queue:
+    x,y = queue.popleft()
+    for i in range(4):
+      nx = x+dx[i]
+      ny = y+dy[i]
+      # 미로 찾기 공간을 벗어난 경우 무시
+      if nx<0 or ny<0 or nx>=n or ny>=m:
+        continue
+      if graph[nx][ny]==0:
+        continue 
+      # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+      if graph[nx][ny] == 1:
+        graph[nx][ny] = graph[x][y] +1
+        queue.append((nx,ny))
+  return graph[n-1][m-1]
+
+print('결과 :',bfs(0,0))
