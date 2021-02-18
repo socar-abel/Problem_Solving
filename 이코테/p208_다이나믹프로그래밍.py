e44c96b753d80fb0f645d9a5fe394156db079ca5
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-02-18 23:47:13 +0900

    Create p208_다이나믹프로그래밍.py

---
+++ 이코테/p208_다이나믹프로그래밍.py
@@ -0,0 +1,66 @@
+'''
+# 8-1 피보나치 함수 소스코드
+def fibo(x):
+  if x==1 or x==2:
+    return 1
+  return fibo(x-1) + fibo(x-2)
+
+print(fibo(4))
+
+# 8-2 피보나치 함수 소스코드 (메모이제시연, 캐싱 기법 사용)
+d = [0]*100
+
+def fibo(x):
+  if x==1 or x==2:
+    return 1
+  if d[x] != 0:
+    return d[x]
+  d[x] = fibo(x-1) + fibo(x-2)
+
+  return d[x]
+
+print(fibo(99))
+
+# 8-3 호출되는 함수 확인
+d = [0]*100
+
+def pibo(x):
+  print('f('+str(x)+')',end=' ')
+  if x==1 or x==2:
+    return 1
+  if d[x]!=0:
+    return d[x]
+  d[x] = pibo(x-1) + pibo(x-2)
+  return d[x]
+pibo(6)
+print()
+
+
+# 8-4 피보나치 수열 소스코드 (바텀업)
+d = [0]*100
+
+d[1]=1
+d[2]=1
+n=99
+
+for i in range(3,n+1):
+  d[i] = d[i-1] + d[i-2]
+print(d[n])
+
+'''
+
+# 2. 1로 만들기
+
+x = int(input())
+d = [0]*30001
+
+for i in range(2,x+1):
+  d[i] = d[i-1] + 1
+  if i%2 == 0:
+    d[i] = min(d[i],d[i//2]+1)
+  if i&3 == 0:
+    d[i] = min(d[i],d[i//3]+1)
+  if i%5 == 0:
+    d[i] = min(d[i],d[i//5]+1)
+
+print(d[x])
