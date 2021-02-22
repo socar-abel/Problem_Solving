a779bfac39a3257bb3d3229a0f18150c4e3f1e45
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-02-22 21:45:43 +0900

    Update p230_최단경로.py

--- 이코테/p230_최단경로.py
+++ 이코테/p230_최단경로.py
@@ -193,7 +193,6 @@ for a in range(1,n+1):
   print()
 
 
-'''
 # (복습) 9-3 플로이드 워셜 알고리즘
 INF = int(1e9)
 
@@ -231,3 +230,86 @@ for a in range(1, n+1):
     else:
       print(graph[a][b],end=' ')
   print()
+
+
+# 2 미래 도시 
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+# 노드의 개수 및 간선의 개수를 입력받기
+n, m = map(int,input().split())
+# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1,n+1):
+  for b in range(1,n+1):
+    if a==b:
+      graph[a][b]=0
+
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(m):
+  a,b = map(int,input().split())
+  graph[a][b]=1
+  graph[b][a]=1
+
+# 거처 갈 노드 X와 최종 목적지 노드 K를 입력받기
+x,k = map(int,input().split())
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1,n+1):
+  for a in range(1,n+1):
+    for b in range(1,n+1):
+      graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
+
+# 수행된 결과를 출력
+distance = graph[1][k]+graph[k][x]
+
+# 도달할 수 없는 경우, -1을 출력
+if distance >= INF:
+  print("-1")
+else:
+  print('distance :',distance)
+
+
+'''
+
+# 3 전보
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, start = map(int,input().split())
+graph = [[] for i in range(n+1)]
+distance = [INF]*(n+1)
+
+for _ in range(m):
+  x,y,z = map(int,input().split())
+  graph[x].append((y,z))
+
+def dijkstra(start):
+  q = []
+  heapq.heappush(q,(0,start))
+  distance[start] = 0
+  while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+      continue
+    for i in graph[now]:
+      cost = dist+i[1]
+
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(q, (cost,i[0]))
+
+dijkstra(start)
+
+count = 0
+max_distance = 0
+for d in distance:
+  if d!=INF :
+    count+=1
+    max_distance = max(max_distance,d)
+
+print(count-1,max_distance)
+
+
