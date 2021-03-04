9faf1a1b4f1be4428623026ebfe6fb587e0c82b5
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-03-04 10:04:27 +0900

    Update 구현2.py

--- 이코테/구현2.py
+++ 이코테/구현2.py
@@ -1,3 +1,4 @@
+'''
 # 예제 4-1 상하좌우 ((이거 tmp1, tmp2가 왜 서로 다르게 나오는 거지 ??))
 # 파이썬에서는 얕은 복사, 깊은 복사가 일어난다고 함. 아직은 잘 모르겠음..
 n = int(input())
@@ -39,3 +40,24 @@ for command in commands:
     print()
 
 print('result:',location)
+'''
+# 4-1 책의 코드 
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+# 상하좌우
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+move_types=['U','D','L','R']
+
+for plan in plans:
+  for i in range(len(move_types)):
+    if plan == move_types[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+  if nx<1 or ny <1 or nx>n or ny>n:
+    continue
+  x, y = nx, ny
+
+print(x,y)
