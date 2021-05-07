1685330f41e92faaccef3eab64f780a15f05b0f2
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-05-07 18:06:23 +0900

    Create 이진탐색bisect.py

---
+++ 잡기술/이진탐색bisect.py
@@ -0,0 +1,9 @@
+# 와 이 좋은걸 지금 알았네 ;;
+
+from bisect import bisect_left, bisect_right
+array = [10,10,10,20,30,30,40,40,40,40,40,60,70,80,80,90]
+
+cutLine = 95
+index1 = bisect_left(array,cutLine)
+index2 = bisect_right(array,cutLine)
+print(index1,index2)
