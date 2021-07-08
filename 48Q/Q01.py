d38abfa3cbab6f21e993fdf7a2c2ca97aef64111
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-07-08 21:21:31 +0900

    이것이 코딩 테스트다 48제

---
+++ 48Q/Q01.py
@@ -0,0 +1,20 @@
+# 이코테 48제 - 그리디 - 모험가 길드
+
+n = int(input())
+array = list(map(int,input().split()))
+
+array.sort() # O(log n)
+
+i = len(array) - 1
+count = 0
+
+while True:
+  if array[i] > i+1 : # array[3] = 4 인 경우 종료
+    break
+  else :
+    temp = array[i]
+    i = i - temp
+    count += 1
+
+print("The number of group :",count)
+  
