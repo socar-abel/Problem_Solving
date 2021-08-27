c95f771771a2efb422e8e3fa88c5edb5c3ae5fbb
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-08-27 18:47:23 +0900

    Update 디스크 컨트롤러.py

--- 프로그래머스/Level3/디스크 컨트롤러.py
+++ 프로그래머스/Level3/디스크 컨트롤러.py
@@ -1,20 +1,20 @@
 import heapq
 def solution(jobs):
-    answer, now, i = 0, 0, 0
+    answer, time, i = 0, 0, 0
     start = -1
     heap = []
     
     while i < len(jobs):
         for j in jobs:
-            if start < j[0] <= now :
+            if start < j[0] <= time :
                 heapq.heappush(heap, (j[1],j[0]))
-        if len(heap) > 0:
+        if heap :
             current = heapq.heappop(heap)
-            start = now
-            now += current[0]
-            answer += (now - current[1])
+            start = time
+            time += current[0]
+            answer += (time - current[1])
             i += 1
         else :
-            now += 1
+            time += 1
             
     return answer // len(jobs)
