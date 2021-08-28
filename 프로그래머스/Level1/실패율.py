1f5b40620f9aefe748a3340c23662158c1cb6e80
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-08-28 16:13:21 +0900

    Create 실패율.py

---
+++ 프로그래머스/Level1/실패율.py
@@ -0,0 +1,27 @@
+# 이진탐색으로 구현 안해도 효율성 테스트 통과는 하는 듯 하다.
+# Level1은 아니고 Level2 정도? 되는 듯
+import bisect
+def solution(N, stages):
+    answer = []
+    stages.sort()
+    failure = [-1] * (N+1)
+    
+    for x in range(1, N+1):
+        left = bisect.bisect_left(stages, x)
+        right = bisect.bisect_right(stages, x)
+        
+        notClear = right - left
+        reach = len(stages) - left
+        if reach == 0: failure[x] = 0
+        else : failure[x] = notClear / reach 
+    
+    enums = []
+    for enum in enumerate(failure):
+        enums.append(enum)
+    
+    enums.sort(key = lambda x : x[1], reverse = True)
+    
+    for enum in enums[:-1]:
+        answer.append(enum[0])
+    
+    return answer
