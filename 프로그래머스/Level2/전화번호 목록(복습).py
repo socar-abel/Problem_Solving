1cde00471bd8f0a1263ce85e88e8a102721972a7
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-09-09 15:58:59 +0900

    Create 전화번호 목록(복습).py

---
+++ 프로그래머스/Level2/전화번호 목록(복습).py
@@ -0,0 +1,11 @@
+# 늘었다.. 제한사항 phone_book 크기가 10^6 인 것을 보고 O(N logN)의 알고리즘을 짜야한다고 바로 생각이 들었고, sort()가 O(N logN) time임을 떠올렸다.
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    arr = []
+    for i in range(1,len(phone_book)):
+        if phone_book[i-1] == phone_book[i][:len(phone_book[i-1])]:
+            return False
+    
+    return answer
