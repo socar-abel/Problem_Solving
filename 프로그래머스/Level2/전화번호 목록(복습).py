f5a41445e90e3c22670810359fd80d0ba54cb246
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-09-09 16:01:01 +0900

    Update 전화번호 목록(복습).py

--- 프로그래머스/Level2/전화번호 목록(복습).py
+++ 프로그래머스/Level2/전화번호 목록(복습).py
@@ -5,7 +5,6 @@ def solution(phone_book):
     phone_book.sort()
 
     for i in range(1,len(phone_book)):
-        if phone_book[i-1] == phone_book[i][:len(phone_book[i-1])]:
-            return False
+        if phone_book[i-1] == phone_book[i][:len(phone_book[i-1])]: return False
     
     return answer
