bf8e844006601093079bb95b34db85bd64d6f802
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-09-09 16:27:30 +0900

    Update 타겟넘버(복습).py

--- 프로그래머스/Level2/타겟넘버(복습).py
+++ 프로그래머스/Level2/타겟넘버(복습).py
@@ -7,10 +7,8 @@ def solution(numbers, target):
         if i == len(numbers):
             if result == target : answer += 1
             return
-            
         dfs(i+1, result + numbers[i])
         dfs(i+1, result - numbers[i])
     
     dfs(0,0)
-    
     return answer
