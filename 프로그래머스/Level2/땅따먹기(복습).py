c0dd507b63db37373736c8f027e26491329720f6
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-09-10 16:17:54 +0900

    Create 땅따먹기(복습).py

---
+++ 프로그래머스/Level2/땅따먹기(복습).py
@@ -0,0 +1,11 @@
+def solution(land):
+    answer = 0
+    dp = [[0]*4 for _ in range(len(land))]
+    dp[0] = land[0]
+    
+    for i in range(1,len(land)):
+        for x in range(4):
+            dp[i][x] = max(dp[i-1][:x]+dp[i-1][x+1:]) + land[i][x]
+    
+    return max(dp[-1])
+
