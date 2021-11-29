99cce8a0465284fe75b27486a1fb5558edd7273d
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-11-29 16:07:15 +0900

    최장 공통 부분 수열 문제. 풀긴 했는데 다시 풀면 또 못 풀것 같다.

---
+++ 백준/DP/9251(LCS).py
@@ -0,0 +1,14 @@
+import sys
+A = sys.stdin.readline().strip()
+B = sys.stdin.readline().strip()
+
+dp = [[0]*(len(B)+1) for _ in range(len(A)+1)]
+
+for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+        if A[i-1] == B[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])
