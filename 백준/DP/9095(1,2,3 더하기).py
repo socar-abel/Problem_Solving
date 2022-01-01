647cae3898172194573f5026784524d2a7d4e076
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2022-01-01 10:40:36 +0900

    Create 9095(1,2,3 더하기).py

---
+++ 백준/DP/9095(1,2,3 더하기).py
@@ -0,0 +1,21 @@
+import sys
+T = int(sys.stdin.readline())
+
+
+def program():
+    n = int(sys.stdin.readline())
+    dp = [0]*(n+1)
+    dp[0] = 1
+
+    for i in range(n+1):
+        if i >= 1:
+            dp[i] += dp[i - 1]
+        if i >= 2:
+            dp[i] += dp[i - 2]
+        if i >= 3:
+            dp[i] += dp[i - 3]
+    print(dp[-1])
+
+
+for _ in range(T):
+    program()
