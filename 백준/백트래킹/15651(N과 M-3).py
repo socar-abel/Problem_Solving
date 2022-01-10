7abb12e1c1c8b593b959d09b794161ca3665b819
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2022-01-10 11:12:52 +0900

    Create 15651(N과 M-3).py

---
+++ 백준/백트래킹/15651(N과 M-3).py
@@ -0,0 +1,20 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+arr = []
+
+
+def dfs(num, path, cnt):
+    if cnt == M:
+        arr.append(path)
+        return
+
+    for i in range(1, N+1):
+        dfs(num, path+' '+str(i), cnt+1)
+
+
+for i in range(1, N+1):
+    dfs(i, str(i), 1)
+
+
+for x in arr:
+    print(x)
