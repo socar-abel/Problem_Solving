225e7c781760861b1cd96c3838203de20291a6af
김상우 <61315014+heyksw@users.noreply.github.com>
2023-01-06 14:52:08 +0900

    heap 구현, 활용

---
+++ swift/프로그래머스/명예의 전당 (1).swift
@@ -0,0 +1,101 @@
+import Foundation
+
+public struct Heap<T> {
+  var nodes: [T] = []
+  let comparer: (T,T) -> Bool
+
+  var isEmpty: Bool {
+      return nodes.isEmpty
+  }
+
+  init(comparer: @escaping (T,T) -> Bool) {
+      self.comparer = comparer
+  }
+
+  func top() -> T? {
+      return nodes.first
+  }
+
+  mutating func push(_ element: T) {
+      var index = nodes.count
+
+      nodes.append(element)
+
+      while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
+          nodes.swapAt(index, (index-1)/2)
+          index = (index-1)/2
+      }
+  }
+
+  mutating func pop() -> T? {
+      guard !nodes.isEmpty else {
+          return nil
+      }
+
+      if nodes.count == 1 {
+          return nodes.removeFirst()
+      }
+
+      let result = nodes.first
+      nodes.swapAt(0, nodes.count-1)
+      _ = nodes.popLast()
+
+      var index = 0
+
+      while index < nodes.count {
+          let left = index * 2 + 1
+          let right = left + 1
+
+          if right < nodes.count {
+              if comparer(nodes[left], nodes[right]),
+                  !comparer(nodes[right], nodes[index]) {
+                  nodes.swapAt(right, index)
+                  index = right
+              } else if !comparer(nodes[left], nodes[index]){
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else if left < nodes.count {
+              if !comparer(nodes[left], nodes[index]) {
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else {
+              break
+          }
+      }
+
+      return result
+  }
+}
+
+extension Heap where T: Comparable {
+    init() {
+        self.init(comparer: <=)
+    }
+}
+
+
+func solution(_ k:Int, _ score:[Int]) -> [Int] {
+    var answer = [Int]()
+    var heap = Heap<Int>(comparer: >=)
+    
+    for s in score {
+        if heap.nodes.count < k {
+            heap.push(s)
+        }
+        else {
+            if s > heap.nodes.min()! {
+                let _ = heap.pop()
+                heap.push(s)
+            }
+        }
+        answer.append(heap.nodes.min()!)
+    }
+    
+    return answer
+}
