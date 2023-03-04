e6898a90b2010bf1d483cf8cd43138fbb2b69121
김상우 <61315014+heyksw@users.noreply.github.com>
2023-03-04 11:11:45 +0900

    dynamic programming

---
+++ swift/프로그래머스/멀리 뛰기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var dp = [Int](repeating: 0, count: n+1)
+    dp[0] = 1
+    dp[1] = 1
+    
+    for i in 2..<n+1 {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    }
+    
+    return dp.last!
+}
